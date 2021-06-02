2effe9e63ab7201ee817c3a3b03b1b0942541b81
Ethan <ethanchristianwayne@gmail.com>
2021-06-02 14:19:31 -0400

    snake file

--- game.js
+++ game.js
@@ -1,11 +1,28 @@
+import { update as updateSnake, draw as drawSnake, SNAKE_SPEED } from './snake.js'
+
 let lastRenderTime = 0
 
 
+
 function main(currentTime) {
-    const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000 
     window.requestAnimationFrame(main)
-    lastRenderTime = currentTime
-    console.log(currentTime)
+    const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000 
+     if (secondsSinceLastRender < 1 / SNAKE_SPEED) return
+    
+    
+    
+    lastRenderTime = currentTime 
+    
+    update()
+    draw()
 }
 
 window.requestAnimationFrame(main)
+
+function update() {
+    updateSnake()
+}
+
+function draw(){
+    drawSnake()
+}
