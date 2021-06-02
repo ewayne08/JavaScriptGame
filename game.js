778304c909832831cf41813ca9ace2cb341b5254
Ethan <ethanchristianwayne@gmail.com>
2021-06-02 13:29:39 -0400

    setting up frames

--- game.js
+++ game.js
@@ -1,5 +1,10 @@
+let lastRenderTime = 0
+
+
 function main(currentTime) {
+    const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000 
     window.requestAnimationFrame(main)
+    lastRenderTime = currentTime
     console.log(currentTime)
 }
 
